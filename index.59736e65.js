73bfbad49f62943fd4dfe22489f6a5beaccfea6b
DimeHetMax <86876206+DimeHetMax@users.noreply.github.com>
2023-05-22 21:08:44 +0000

    Deploying to gh-pages from @ DimeHetMax/goit-js-hw-11@f120d8570852f6a5d6ad5480276b226006d7c9bc 🚀

--- index.59736e65.js
+++
@@ -1,2 +0,0 @@
-const e={form:document.querySelector(".search-form"),galleryEl:document.querySelector(".gallery")};console.log(e.galleryEl);
-//# sourceMappingURL=index.59736e65.js.map
--- index.ba924d2a.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={form:document.querySelector(".search-form"),galleryEl:document.querySelector(".gallery")};console.log(e.galleryEl)}();
-//# sourceMappingURL=index.ba924d2a.js.map
