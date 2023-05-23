5d595baef1600d8cb32832fdd7e8860b56b13d97
DimeHetMax <86876206+DimeHetMax@users.noreply.github.com>
2023-05-23 21:23:41 +0300

    6

--- src/index.js
+++ src/index.js
@@ -21,7 +21,9 @@ const refs = {
     galleryEl: document.querySelector(".gallery"),
     loadButton: document.querySelector(".load-more")
 }
-refs.loadButton.classList.add("hidden");
+
+// refs.loadButton.classList.add("hidden");
+hideLoadButton()
 
 let page = 1;
 let searchQuery;
@@ -36,15 +38,18 @@ function onButton(event){
     event.preventDefault()
     const form = event.currentTarget
     searchQuery = form.elements.searchQuery.value.trim();
-    
+
     if(searchQuery === ""){
         return
     }
     clearPictureList()
     page = 1
     refs.loadButton.classList.remove("hidden");
+
     getPictures(searchQuery, page)
     .then(renderHTML)
+    .then(initializeLightbox)
+    .then(smootheSlide)
     
     refs.form.elements.searchQuery.value = "";
 }
@@ -52,7 +57,8 @@ async function getPictures(query, pageNum){
     try{
         return await axios.get(`${URL}?key=${KEY}&q=${query}&page=${pageNum}&per_page=39&orientation=horizontal&image_type=photo&safesearch=true`);
     }catch(error){
-        refs.loadButton.classList.add("hidden");
+        // refs.loadButton.classList.add("hidden");
+        hideLoadButton()
         if(error.message === "Request failed with status code 400"){
           Notify.failure("We're sorry, but you've reached the end of search results.")
         }else{
@@ -64,7 +70,8 @@ async function getPictures(query, pageNum){
 function renderHTML({data}){
     const {hits} = data;
     if(data.total === 0){
-        refs.loadButton.classList.add("hidden");
+        // refs.loadButton.classList.add("hidden");
+        hideLoadButton()
         return Notify.info("Sorry, there are no images matching your search query. Please try again.")
     }
     Notify.success(`Hooray! We found ${data.totalHits} images.`)
@@ -72,23 +79,24 @@ function renderHTML({data}){
    const photoCard = hits.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) =>{
         return `
         <div class="photo-card">
-        <a href="${largeImageURL}" class="large-img-link">
-            <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-        </a>
-        <div class="info">
-            <p class="info-item">
-            <b>Likes: ${likes}</b>
-            </p>
-            <p class="info-item">
-            <b>Views: ${views}</b>
-            </p>
-            <p class="info-item">
-            <b>Comments: ${comments}</b>
-            </p>
-            <p class="info-item">
-            <b>Downloads: ${downloads}</b>
-            </p>
-        </div>
+            <a href="${largeImageURL}" class="large-img-link">
+                <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+            </a>
+            <div class="info">
+                <p class="info-item">
+                <b>Likes: ${likes}</b>
+                </p>
+                <p class="info-item">
+                <b>Views: ${views}</b>
+                </p>
+                <p class="info-item">
+                <b>Comments: ${comments}</b>
+                </p>
+                <p class="info-item">
+                <b>Downloads: ${downloads}</b>
+                </p>
+            </div>
+            
         </div>
         `
     }).join(" ");
@@ -98,8 +106,10 @@ function renderHTML({data}){
 
 function addMarkUpOnLoadButton(){
    const pageNumber = updatePage()
-   getPictures(searchQuery, pageNumber).then(renderHTML)
-   refresh()
+   getPictures(searchQuery, pageNumber)
+   .then(renderHTML)
+   .then(initializeLightbox)
+   .then(smootheSlideLoadMore)
 }
 
 function updateMarkUp(markup){
@@ -114,6 +124,24 @@ function onError(error){
 function updatePage(){
     return page +=1
 }
+function initializeLightbox() {
+   return new SimpleLightbox('.photo-card .large-img-link', {captions: true, captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250});
+  }
+function smootheSlide(){
+    const { height: cardHeight } = document.querySelector(".gallery").firstElementChild.getBoundingClientRect();
+    window.scrollBy({
+      top: cardHeight / 2,
+      behavior: "smooth",
+    });
+}
+function smootheSlideLoadMore(){
+    const { height: cardHeight } = document.querySelector(".gallery").firstElementChild.getBoundingClientRect();
+    window.scrollBy({
+      top: cardHeight * 1.5,
+      behavior: "smooth",
+    })
+}
 
-let gallery = new SimpleLightbox('.gallery .large-img-link');
-gallery.on('show.simplelightbox', function () {});
+function hideLoadButton(){
+    refs.loadButton.classList.add("hidden");
+}
